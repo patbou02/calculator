d4fcd2d4efdc92e8dbc8a6801c08c7a8a5dc408a
Patrick Boutin <patrick.boutin@roberthalf.com>
2020-11-07 17:37:57 -0800

    Starting update to HTML and JS comments for operations.

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,23 @@ import 'normalize.css';
 
 document.addEventListener('DOMContentLoaded', () => {
 
+  // 1. detect keypress
+  const calculator = document.querySelector('.calculator__keys');
+  const keys = calculator.querySelectorAll('button');
+
+  keys.forEach((key) => {
+    //console.log(key.dataset.key);
+    key.addEventListener('click', function (e) {
+      console.log(e.target.dataset.key);
+    });
+  });
+
+  // 2. operations:
+  // addition
+  // subtraction
+  // multiplication
+  // division
+  // percentage
+  // clear/reset
+
 });
